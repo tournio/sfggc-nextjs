bc9d8d07ee8625b1315b701931317676ca9885a4
Scott Stebleton <scott@stebleton.net>
2023-07-16 13:58:30 -0400

    add tournament logo

--- src/components/layout/footer.js
+++ src/components/layout/footer.js
@@ -1,21 +1,31 @@
 // import Image from "next/image";
 // import logo from '../../images/damit.png';
 
+import Image from "next/image";
+import logo from '../../images/sfggc-logo.jpg';
+
 const Footer = () => {
   return (
     <div>
       <hr />
-      <p className={'d-flex justify-content-center pb-3'}>
-        <span>
-          &copy; 2023&nbsp;
-        </span>
-        <span className={'d-md-none pe-2'}>
-          SF Golden Gate Classic
-        </span>
-        <span className={'d-none d-md-inline pe-2'}>
-          San Francisco Golden Gate Classic
-        </span>
-      </p>
+      <div className={`row d-flex justify-content-center flex-wrap pb-0`}>
+        <div className={`col-6`}>
+          <Image src={logo}
+                 alt={'Tournament logo'}
+                 className={`img-fluid logo-image`}/>
+        </div>
+        <p className={`col-12 text-center pt-3`}>
+          <span>
+            &copy; 2023&nbsp;
+          </span>
+            <span className={'d-md-none pe-2'}>
+            SF Golden Gate Classic
+          </span>
+            <span className={'d-none d-md-inline pe-2'}>
+            San Francisco Golden Gate Classic
+          </span>
+        </p>
+      </div>
     </div>
   )
 }
