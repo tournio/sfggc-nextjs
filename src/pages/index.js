adc2ca0090fdf11028f79365379c311f157a5391
Scott Stebleton <scott@stebleton.net>
2023-12-19 10:04:09 -0500

    add mission statement

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,12 +3,14 @@ import Hero from "../components/Hero/Hero";
 import Location from "../components/Location/Location";
 import Schedule from '../components/Schedule/Schedule';
 import SanFrancisco from "../components/SanFrancisco/SanFrancisco";
+import Mission from "../components/Mission/Mission";
 
 const MainPage = () => {
   return (
     <div>
 
       <Hero/>
+      <Mission/>
       <Location/>
       <Schedule/>
       <SanFrancisco full={false}/>
