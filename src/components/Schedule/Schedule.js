ba607a7444fe497081267bb8c0271c2b939278d9
Scott Stebleton <scott@stebleton.net>
2024-01-20 09:42:35 +0100

    add no-tap pricing

--- src/components/Schedule/Schedule.js
+++ src/components/Schedule/Schedule.js
@@ -15,7 +15,7 @@ const Schedule = () => {
         {
           name: '9-pin No-tap',
           detail: 'Classic Bowling Center',
-          more: 'Open to all, including non-tournament bowlers',
+          more: '$30 entry fee, open to all',
           time: '8pm - 10pm',
         },
       ]
